67e7f8d98e106fd55e7c539ddc03c1d9d4960854
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2023-05-11 12:16:11 +0200

    feat: Creat Pull Request class

---
+++ tests/test_pull_request.py
@@ -0,0 +1,18 @@
+from uuid import uuid4
+import pytest
+from pull_request import PullRequest
+
+
+def test_update_auto_body_preservers_custom_text():
+    custom_text = """Some Custom Text
+That I want to preserve"""
+
+    pr_body = f"{custom_text}\n{PullRequest.DELIMITER}\nPr Auto Body"
+    pr = PullRequest(str(uuid4()), pr_body)
+
+    new_auto_body = "New auto body"
+    print(pr.body)
+    pr.update_auto_body(new_auto_body)
+    print(pr.body)
+    assert new_auto_body in pr.body
+    assert pr.body.startswith(custom_text)
